2972bd136e8df91d9e3b6ba9606519769d67d8c6
unknown <aelise17264@gmail.com>
2020-10-01 11:56:29 -0600

    update

--- api/server.js
+++ api/server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const server = express()
 
 server.get('/', (req, res) => {
-    res.status(200).json({hello: 'node 34', environment: NODE_ENV})
+    res.status(200).json({ environment: 'NODE_ENV'})
 })
 
 module.exports = server
